73cf6b6b6bb99b7430bff0dd13466fd2c36e1c7e
flkt <magaly@flkt.mx>
2020-06-12 13:12:52 -0500

    Updates

---
+++ precache-manifest.6706c9b4f57b54b244428b80d6990562.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "917f579912a0c7b20914e6a1b6dd2f91",
+    "url": "/CodeandoMexico/covid-estados/index.html"
+  },
+  {
+    "revision": "1b8069fe23c1e97efe8f",
+    "url": "/CodeandoMexico/covid-estados/static/css/main.41b645dd.chunk.css"
+  },
+  {
+    "revision": "1962a8cbb1c2f4f94077",
+    "url": "/CodeandoMexico/covid-estados/static/js/2.e6ba8ec4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/CodeandoMexico/covid-estados/static/js/2.e6ba8ec4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b8069fe23c1e97efe8f",
+    "url": "/CodeandoMexico/covid-estados/static/js/main.3c03d22a.chunk.js"
+  },
+  {
+    "revision": "c6799a816c00eb1b2810",
+    "url": "/CodeandoMexico/covid-estados/static/js/runtime-main.5aaace49.js"
+  },
+  {
+    "revision": "c7af8d3d6c0e00326e772c669b294f9e",
+    "url": "/CodeandoMexico/covid-estados/static/media/android.c7af8d3d.svg"
+  },
+  {
+    "revision": "ee0404f0ac79cec5be36be22a479e70c",
+    "url": "/CodeandoMexico/covid-estados/static/media/apple.ee0404f0.svg"
+  },
+  {
+    "revision": "a170074293f27dd157fe3df49e95bdba",
+    "url": "/CodeandoMexico/covid-estados/static/media/cat.a1700742.svg"
+  },
+  {
+    "revision": "869263236ede643a1022b2768d36428c",
+    "url": "/CodeandoMexico/covid-estados/static/media/codeando.86926323.svg"
+  },
+  {
+    "revision": "88b693a39dc5d31a39658557005eb310",
+    "url": "/CodeandoMexico/covid-estados/static/media/codeandomexico.88b693a3.svg"
+  },
+  {
+    "revision": "9c1bdd82df8588108f5918b3b087aa59",
+    "url": "/CodeandoMexico/covid-estados/static/media/docs.9c1bdd82.svg"
+  },
+  {
+    "revision": "684f0a9a13cd09cdc474e0a42c8d5e0f",
+    "url": "/CodeandoMexico/covid-estados/static/media/home.684f0a9a.svg"
+  },
+  {
+    "revision": "6bd8ce182df8f494b369fbe20440bf62",
+    "url": "/CodeandoMexico/covid-estados/static/media/hospital.6bd8ce18.svg"
+  },
+  {
+    "revision": "c766b93079746b0e3edc8c080604c560",
+    "url": "/CodeandoMexico/covid-estados/static/media/ilovehome.c766b930.svg"
+  },
+  {
+    "revision": "b629a41aa4ce337fcca6d4ee624f448b",
+    "url": "/CodeandoMexico/covid-estados/static/media/methodology.b629a41a.svg"
+  },
+  {
+    "revision": "062d38e3530f6a151db5786ef7b0ba31",
+    "url": "/CodeandoMexico/covid-estados/static/media/ndi.062d38e3.png"
+  },
+  {
+    "revision": "9ac0bc31d3a6ced1e8aabfb300a89803",
+    "url": "/CodeandoMexico/covid-estados/static/media/project.9ac0bc31.svg"
+  },
+  {
+    "revision": "aafdda80e5fb2b3e1bddce0ea7701dc4",
+    "url": "/CodeandoMexico/covid-estados/static/media/sms.aafdda80.svg"
+  },
+  {
+    "revision": "ddad123cfad7bba16bee9258b9356275",
+    "url": "/CodeandoMexico/covid-estados/static/media/telephone.ddad123c.svg"
+  },
+  {
+    "revision": "d2f3f5fa3b96d0d1dc6f9cd78ec568ff",
+    "url": "/CodeandoMexico/covid-estados/static/media/telephone2.d2f3f5fa.svg"
+  },
+  {
+    "revision": "4066e7fc20378f13dfb86159f1d04453",
+    "url": "/CodeandoMexico/covid-estados/static/media/web.4066e7fc.svg"
+  },
+  {
+    "revision": "ce50648047c9799c210f5fd0d487d5eb",
+    "url": "/CodeandoMexico/covid-estados/static/media/whatsapp.ce506480.svg"
+  }
+]);
