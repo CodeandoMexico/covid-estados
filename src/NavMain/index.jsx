7a93837049c08aee222aadc315edb3b84f68daaf
flkt <magaly@flkt.mx>
2020-06-18 23:56:11 -0500

    link to home update. external links fixed

--- src/NavMain/index.jsx
+++ src/NavMain/index.jsx
@@ -9,13 +9,13 @@ import './nav.scss';
 function NavBar() {
   return (
     <header className="nav-main">
-      <NavLink to="/"><img className="nav-codeando" src={logo} alt="codeando mexico" /></NavLink>
+      <NavLink to="/covid-estados/"><img className="nav-codeando" src={logo} alt="codeando mexico" /></NavLink>
       <div className="nav-sobre">
         <NavLink className="nav-link" to="/sobre-el-proyecto">Sobre el proyecto</NavLink>
         <div>
-          <NavLink to="https://www.facebook.com/CodeandoMexico/"><img className="nav-redes" src={facebook} alt="facebook" /></NavLink>
-          <NavLink to="https://twitter.com/CodeandoMexico"><img className="nav-redes" src={twitter} alt="twitter" /></NavLink>
-          <NavLink to="https://github.com/CodeandoMexico/covid-estados"><img className="nav-redes" src={github} alt="github" /></NavLink>
+          <a href="https://www.facebook.com/CodeandoMexico" target="_blank" rel="noopener noreferrer"><img className="nav-redes" src={facebook} alt="facebook" /></a>
+          <a href="https://twitter.com/CodeandoMexico" target="_blank" rel="noopener noreferrer"><img className="nav-redes" src={twitter} alt="twitter" /></a>
+          <a href="https://github.com/CodeandoMexico/covid-estados" target="_blank" rel="noopener noreferrer"><img className="nav-redes" src={github} alt="github" /></a>
         </div>
       </div>
     </header>
