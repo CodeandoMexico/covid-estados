906d3e98414ee36ac2ad1bf1ba9fc18ac9af4292
Yarov Rivera <yarov.rivera@neximo.mx>
2020-06-29 09:22:05 -0500

    add util google analytics

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import React from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { getEstados } from "./actions";
-import { BrowserRouter, Switch, Redirect, Route} from "react-router-dom";
-import { createBrowserHistory } from "history";
+import { BrowserRouter, Switch, Redirect, Route } from "react-router-dom";
+
 import Home from "./Home";
 import Detail from "./Detail";
 import About from "./About";
 import NavMain from "./NavMain";
-import Footer from './Footer';
+import Footer from "./Footer";
 import "./App.scss";
 
+import GA from "./utils/GoogleAnalytics";
+
 class App extends React.Component {
-  
   componentDidMount() {
     const { getEstados } = this.props.actions;
     getEstados();
@@ -21,7 +22,8 @@ class App extends React.Component {
   render() {
     const customHistory = createBrowserHistory();
     return (
-      <BrowserRouter history={customHistory}>
+      <BrowserRouter>
+        {GA.init() && <GA.RouteTracker />}
         <NavMain />
         <Switch>
           <Redirect from="/home" to="/" />
