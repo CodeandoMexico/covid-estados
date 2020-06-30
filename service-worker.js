f9ec54712a70cc3a3c0a0e6074e90b76a0aaadd4
Rodolfo Ferro <rodolfoferroperez@gmail.com>
2020-06-29 20:45:54 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid-estados/precache-manifest.999fc17b1beb9f16ebb4040b9fa84be0.js"
+  "/covid-estados/precache-manifest.ee02b53910eabb532e4921b56d9e5f99.js"
 );
 
 self.addEventListener('message', (event) => {
